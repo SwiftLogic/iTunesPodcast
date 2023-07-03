88aff8e97b4e5a4fc4baa9357e212d3dce716315
Samuel Uyigue <Samuel.uyigue@gmail.com>
2023-07-03 08:35:20 -0500

    EpisodesController tableview scaffold

--- iTunesPodcast/Core/Search/Controllers/EpisodesController.swift
+++ iTunesPodcast/Core/Search/Controllers/EpisodesController.swift
@@ -22,19 +22,49 @@ class EpisodesController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         setUpNavItems()
-        setUpViews()
+        setUpTableView()
     }
     
     // MARK: - Properties
     private let podcast: Podcast
+    static let cellId = "EpisodesCellId"
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
     
     
     // MARK: - Methods
-    private func setUpViews() {
-        
+    private func setUpViews() {}
+    
+    private func setUpTableView() {
+        view.addSubview(tableView)
+        tableView.fillSuperview()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: Self.cellId)
     }
     
     private func setUpNavItems() {
         navigationItem.title = podcast.trackName ?? ""
+    }
+}
+
+
+extension EpisodesController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Self.cellId, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 110
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
     }
 }
